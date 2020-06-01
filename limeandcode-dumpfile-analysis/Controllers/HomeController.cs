62d93a04d96f2b6c477a44c7bc622acb3113b729
hyunjae-lee-pushpay <hyunjae.lee@pushpay.com>
2020-06-01 22:12:07 +1200

    Buggy web application demo

---
+++ limeandcode-dumpfile-analysis/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using limeandcode_dumpfile_analysis.Models;
+using System.Threading;
+using System.Text;
+
+namespace limeandcode_dumpfile_analysis.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		public IActionResult BuggyAction()
+		{
+			StringBuilder sb = new StringBuilder();
+			int i = 0;
+			while (true)
+			{
+				i++;
+
+				if (i == 1000)
+				{
+					break;
+				}
+				Thread.Sleep(1000);
+			}
+
+			return new JsonResult(sb.ToString());
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ limeandcode-dumpfile-analysis/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace limeandcode_dumpfile_analysis.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
